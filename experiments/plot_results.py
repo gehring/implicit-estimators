997ee57600ea89226b5d6842f9aec80bd57f8745
gehring <clement.gehring@gmail.com>
2021-01-14 16:24:33 -0500

    Added plotting utilities Added plotting script for current results

---
+++ experiments/plot_results.py
@@ -0,0 +1,99 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+from experiments import utils
+
+sns.set_theme()
+
+results_path = {
+    "implicit": {
+        "chain": "~/results/chain/implicit/202101111222",
+        "four_rooms": "~/results/four_rooms/implicit/202101121451",
+        "mountain_car": "~/results/mountaincar/implicit/202101121507",
+    },
+    "explicit": {
+        "chain": "~/results/chain/explicit/202101051403",
+        "four_rooms": "~/results/four_rooms/explicit/202101051401",
+        "mountain_car": "~/results/mountaincar/explicit/202101121604",
+    }
+}
+
+best_hparams = {
+    "explicit": {
+        "chain": [{"BATCH_SIZE": 1, "LEARNING_RATE": 1.},
+                  {"BATCH_SIZE": 5, "LEARNING_RATE": 2.},
+                  {"BATCH_SIZE": 25, "LEARNING_RATE": 2.}],
+        "four_rooms": [{"BATCH_SIZE": 1, "LEARNING_RATE": 1.},
+                       {"BATCH_SIZE": 5, "LEARNING_RATE": 2.},
+                       {"BATCH_SIZE": 25, "LEARNING_RATE": 2.}],
+        "mountain_car": [{"LEARNING_RATE": 2.}],
+    },
+    "implicit": {
+        "chain": [{"BATCH_SIZE": 1, "MDP_MODULE_DISCOUNT": 0.8, "LEARNING_RATE": 0.25},
+                  {"BATCH_SIZE": 1, "MDP_MODULE_DISCOUNT": 0.9, "LEARNING_RATE": 0.125},
+                  {"BATCH_SIZE": 5, "MDP_MODULE_DISCOUNT": 0.8, "LEARNING_RATE": 0.5},
+                  {"BATCH_SIZE": 5, "MDP_MODULE_DISCOUNT": 0.9, "LEARNING_RATE": 0.25},
+                  {"BATCH_SIZE": 25, "MDP_MODULE_DISCOUNT": 0.8, "LEARNING_RATE": 0.5},
+                  {"BATCH_SIZE": 25, "MDP_MODULE_DISCOUNT": 0.9, "LEARNING_RATE": 0.25},
+                  {"MDP_MODULE_DISCOUNT": 0.95, "LEARNING_RATE": 0.0625}],
+        "four_rooms": [{"BATCH_SIZE": 1, "MDP_MODULE_DISCOUNT": 0.8, "LEARNING_RATE": 0.25},
+                       {"BATCH_SIZE": 1, "MDP_MODULE_DISCOUNT": 0.9, "LEARNING_RATE": 0.25},
+                       {"BATCH_SIZE": 1, "MDP_MODULE_DISCOUNT": 0.95, "LEARNING_RATE": 0.125},
+                       {"BATCH_SIZE": 5, "LEARNING_RATE": 0.5},
+                       {"BATCH_SIZE": 25, "LEARNING_RATE": 0.5}],
+        "mountain_car": [{"BATCH_SIZE": 1, "MDP_MODULE_DISCOUNT": 0.8, "LEARNING_RATE": 0.5},
+                         {"BATCH_SIZE": 1, "MDP_MODULE_DISCOUNT": 0.9, "LEARNING_RATE": 0.5},
+                         {"BATCH_SIZE": 1, "MDP_MODULE_DISCOUNT": 0.95, "LEARNING_RATE": 0.25},
+                         {"BATCH_SIZE": 5, "LEARNING_RATE": 0.5},
+                         {"BATCH_SIZE": 25, "LEARNING_RATE": 0.5}],
+    }
+}
+
+
+def plot_domain_results(domain, ylim):
+    # load and plot implicit chain results
+    implicit_df = utils.load_results(results_path["implicit"][domain])
+    fig = utils.plot_results(
+        data=implicit_df,
+        ylim=ylim,
+        best_hparams=best_hparams["implicit"]["chain"],
+    )
+    fig.savefig(f"implicit_{domain}_norm.pdf")
+
+    # load and plot explicit chain results
+    explicit_df = utils.load_results(results_path["explicit"][domain])
+    fig = utils.plot_results(
+        data=explicit_df,
+        ylim=ylim,
+        best_hparams=best_hparams["explicit"]["chain"],
+    )
+    fig.savefig(f"explicit_{domain}_norm.pdf")
+
+    # filter by best hyper-parameters
+    best_implicit = utils.compute_best_mask(implicit_df, best_hparams["implicit"][domain])
+    best_explicit = utils.compute_best_mask(explicit_df, best_hparams["explicit"][domain])
+
+    # plot comparisons for the chain results
+    fig = utils.plot_comparison(
+        implicit_df[best_implicit],
+        explicit_df[best_explicit],
+        y="residual_norm",
+    )
+    fig.savefig(f"compare_{domain}_norm.pdf")
+
+    fig = utils.plot_comparison(
+        implicit_df[best_implicit],
+        explicit_df[best_explicit],
+        y="res_ones_norm",
+    )
+    fig.savefig(f"compare_{domain}_ones_norm.pdf")
+
+
+# load and plot chain results
+plot_domain_results("chain", ylim=(0., 40.))
+
+# load and plot four rooms results
+plot_domain_results("four_rooms", ylim=(0., 5.))
+
+# load and plot mountain car results
+plot_domain_results("mountain_car", ylim=(0., 1900.))
--- experiments/utils.py
+++ experiments/utils.py
@@ -6,6 +6,8 @@ import os
 
 import pandas as pd
 
+import seaborn as sns
+
 
 def load_run_v1(run_dir, config_dir):
     log_path = os.path.join(run_dir, "logs.csv")
@@ -64,3 +66,57 @@ def load_results(result_dir, config_dir=None):
         df = df.merge(joblogs)
 
     return df
+
+
+def compute_best_mask(data, best_hparams):
+    """Compute mask to select data corresponding to best hyper-parameters.
+
+    `best_hparams` is assumed to be a list of combinations of best hyper-parameters. The returned
+    mask is the `OR` of the mask corresponding to each combination. A mask for a given combination
+    is corresponds to the `AND` when selecting for `NAME == VALUE` for each name/value pair.
+    """
+    best_mask = False
+    for term in best_hparams:
+        term_mask = True
+        for name, value in term.items():
+            term_mask &= data[name] == value
+        best_mask |= term_mask
+    return best_mask
+
+
+def plot_results(data, ylim=None, best_hparams=None, y="residual_norm", hue="LEARNING_RATE"):
+    facet_kws = {"margin_titles": True}
+    if ylim is not None:
+        facet_kws["ylim"] = ylim
+
+    style = None
+    if best_hparams:
+        style = "BEST"
+        data = data.copy()
+        data[style] = compute_best_mask(data, best_hparams)
+
+    return sns.relplot(
+        data=data,
+        x="timestep",
+        y=y,
+        hue=hue,
+        style=style,
+        row="MDP_MODULE_DISCOUNT",
+        col="BATCH_SIZE",
+        kind="line",
+        ci="sd",
+        facet_kws=facet_kws,
+    )
+
+
+def plot_comparison(implicit_data, explicit_data, y="residual_norm", ylim=None):
+    label_name = "Parameterization"
+
+    implicit_data = implicit_data.copy()
+    implicit_data[label_name] = "implicit"
+
+    explicit_data = explicit_data.copy()
+    explicit_data[label_name] = "explicit"
+
+    data = pd.concat((implicit_data, explicit_data), ignore_index=True)
+    return plot_results(data, ylim=ylim, y=y, hue=label_name)
